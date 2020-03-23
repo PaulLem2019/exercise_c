94adc233f3cdb9bfb6fc16bf5b5d28a9a32720e1
PaulLem2019 <46632436+PaulLem2019@users.noreply.github.com>
2020-03-23 21:31:30 +0300

    Update SimpleNumber.c
    correct work program f
    value less 2 is not correct

--- SimpleNumber.c
+++ SimpleNumber.c
@@ -9,26 +9,31 @@ int main ()
 	int val, sum;
 	double prom1, prom2;
 	int i;
-	printf ("enter value\n");
+	printf ("enter value from 2 to ...\n");
 	scanf ("%d", &val);
-	sum = 0;
-	for (i=2; i<val; i++)
+	if (val>1)
 	{
-		prom1 = (double) (val)/(double)(i);
-		prom2 = (int) (val/i);
-		if (prom1 == prom2)
+		sum = 0;
+		for (i=2; i<val; i++)
 		{
-			sum = 1;
-			break;
+			prom1 = (double) (val)/(double)(i);
+			prom2 = (int) (val/i);
+			if (prom1 == prom2)
+			{
+				sum = 1;
+				break;
+			}
+		}
+		if (sum)
+		{
+			printf ("value is not simple %d\n", val);
+		}
+		else
+		{
+			printf ("it's simple value %d sum %d\n", val, sum);
 		}
 	}
-	if (sum)
-	{
-		printf ("value is not simple %d\n", val);
-	}
 	else
-	{
-		printf ("it's simple value %d sum %d\n", val, sum);
-	}
+		printf ("Value is not correct\n");
 }
 			
